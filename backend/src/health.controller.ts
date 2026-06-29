5400d204c8ea86a85f6df0ea2845e95dcb27bdd0
aLEAsu <alejovidal2606@gmail.com>
2026-06-29 18:12:07 -0500

    Update health.controller.ts

--- backend/src/health.controller.ts
+++ backend/src/health.controller.ts
@@ -1,10 +1,9 @@
-
 import { Controller, Get } from '@nestjs/common';
 
 @Controller()
-export class AppController {
+export class HealthController {
   @Get('health')
   health() {
-    return { status: 'ok' };
+    return { status: 'ok', timestamp: new Date().toISOString() };
   }
 }
